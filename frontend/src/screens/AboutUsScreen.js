fc6a366bff426ad27ee5568797321a2a976165c8
Jarl Briones <jarljbriones@gmail.com>
2023-01-01 05:42:42 +0800

    "added About Us"

--- frontend/src/screens/AboutUsScreen.js
+++ frontend/src/screens/AboutUsScreen.js
@@ -7,30 +7,67 @@ export default function AboutUsScreen() {
 
   return (
     <div>
-      <div class="about">
-        <div class="inner-section">
-          <h1>Jarl</h1>
-          <p class="text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
+      <div className="about">
+        <div className="inner-section">
+          <h1>Gab</h1>
+          <p className="text">
+            I am currently pursuing a degree in computer science at University
+            of St. La Salle. I have always been fascinated by the power of
+            technology to solve problems and make a difference in the world. In
+            my free time, I enjoy tinkering with code and building my own
+            websites and applications. I am also active in my school's computer
+            science club, where I have had the opportunity to work on
+            collaborative projects and attend industry events. After graduation,
+            I hope to work at a tech company where I can continue to learn and
+            grow as a developer.
           </p>
         </div>
       </div>
-      <div class="about1">
-        <div class="inner-section1">
-          <h1>Gab</h1>
-          <p class="text1">MAMA</p>
+      <div className="about1">
+        <div className="inner-section1">
+          <h1>Jarl</h1>
+          <p className="text1">
+            I am a computer science student at University of St. La Salle, and I
+            have always been passionate about technology and problem-solving. In
+            my free time, I enjoy coding and building new projects. I am
+            particularly interested in artificial intelligence and machine
+            learning, and I hope to work in these fields after graduation.
+            Outside of my studies, I am an avid gamer and enjoy spending time
+            with my friends. I am excited to continue learning and growing as a
+            computer scientist.
+          </p>
         </div>
       </div>
-      <div class="about2">
-        <div class="inner-section2">
+      <div className="about2">
+        <div className="inner-section2">
           <h1>Yuji</h1>
-          <p class="text2">PAPA</p>
+          <p className="text2">
+            I am currently studying computer science at University of St. La
+            Salle. I have always been interested in technology and enjoy
+            learning about the latest developments in the field. In my free
+            time, I like to work on personal programming projects and
+            participate in online coding challenges. I am also a member of my
+            school's computer science club, where I have had the opportunity to
+            work on group projects and attend industry events. After graduation,
+            I hope to secure a job as a software engineer at a company where I
+            can continue to learn and grow as a developer.
+          </p>
         </div>
       </div>
-      <div class="about1">
-        <div class="inner-section1">
+      <div className="about3">
+        <div className="inner-section3">
           <h1>Kayla</h1>
-          <p class="text1">Ate</p>
+          <p className="text3">
+            I am currently pursuing a degree in Computer Science at a local
+            university. My passion for technology began at a young age, and I
+            have always been fascinated by how computers work and the endless
+            possibilities they offer. In my free time, I enjoy tinkering with
+            code and building projects to expand my knowledge and skills. After
+            graduation, I hope to work as a software engineer at a reputable
+            company where I can continue to learn and grow in my career. I am
+            excited to see where my studies in computer science will take me in
+            the future.
+          </p>
         </div>
       </div>
     </div>
